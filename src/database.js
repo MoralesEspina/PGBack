4be1f808990b32dfddc9a0e30a70bccb9df29a7f
DESKTOP-4PPG5ET\Esdras <moralesespina92@gmail.com>
2022-09-23 22:37:16 -0600

    Conexion con MongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost/gasdanerydb")
+  .connect(process.env.MONGODB_URI)
   .then((db) => console.log("DB is connected"))
   .catch((error) => console.log(error));
